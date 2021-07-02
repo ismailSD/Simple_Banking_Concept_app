b8966a51a220cd7a3675f94b4eb99470917e39fe
ismail adam <ismailadam3136@gmail.com>
2021-07-02 15:55:56 +0100

    edit read me contnet

--- main.go
+++ main.go
@@ -168,11 +168,3 @@ func displayInstructions(){
 		"7: exit program\n" +
 		"Select an action:")
 }
-//Simple banking concept program in Go
-//1.	Deposit money
-//2.	Withdraw money
-//3.	Show current balance
-//4.	Set withdrawal limit per day
-//5.	Show statement
-//6.	Print statement to CSV file
-//7.	Exit program
